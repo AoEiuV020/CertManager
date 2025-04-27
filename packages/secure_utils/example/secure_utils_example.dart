1cd700aa47d184d5a864dedd1a259383be6e782f
AoEiuV020 <aoeiuv020@gmail.com>
2025-04-27 16:38:53 +0800

    create package

---
+++ packages/secure_utils/example/secure_utils_example.dart
@@ -0,0 +1,6 @@
+import 'package:secure_utils/secure_utils.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/secure_utils/test/secure_utils_test.dart
@@ -0,0 +1,16 @@
+import 'package:secure_utils/secure_utils.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
